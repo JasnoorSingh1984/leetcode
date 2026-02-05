9c484061b027a74859aa19d4911e9c1152460df1
Jasnoor Singh <jasnoortoor1984@gmail.com>
2026-02-05 11:41:27 +0530

    Time: 8 ms (10.03%), Space: 52.8 MB (5.21%) - LeetHub

---
+++ 0051-n-queens/0051-n-queens.java
@@ -0,0 +1,97 @@
+class Solution {
+    public List<List<String>> solveNQueens(int n) {
+        char[][] arr=new char[n][n];
+        for (int i=0;i<n;i++){
+            for (int j=0;j<n;j++){
+                arr[i][j]='.';
+            }
+        }
+
+        List<List<String>> ans=new ArrayList<>();
+        print(arr,0,ans);
+        return ans;
+    }
+
+    public void print(char[][] arr,int rows,List<List<String>> ans){
+        int n=arr.length;
+
+        if (rows==n){
+            List<String> l=new ArrayList<>();
+            for (int i=0;i<n;i++){
+                String str="";
+                for (int j=0;j<n;j++){
+                    str+=arr[i][j];
+                }
+                l.add(str);
+            }
+            ans.add(l);
+            return;
+        }
+
+        for (int i=0;i<n;i++){
+            if (safe(arr,rows,i)){
+                arr[rows][i]='Q';
+                print(arr,rows+1,ans);
+                arr[rows][i]='.';
+            }
+        }
+    }
+
+    public boolean safe(char[][] arr,int rows,int cols){
+        int n=arr.length;
+
+        for (int i=0;i<n;i++){
+            if (arr[rows][i]=='Q'){
+                return false;
+            }
+        }
+
+        for (int i=0;i<n;i++){
+            if (arr[i][cols]=='Q'){
+                return false;
+            }
+        }
+
+        int i=rows;
+        int j=cols;
+        while (i>=0 && j<n){
+            if (arr[i][j]=='Q'){
+                return false;
+            }
+            i--;
+            j++;
+        }
+
+        i=rows;
+        j=cols;
+        while (i<n && j<n){
+            if (arr[i][j]=='Q'){
+                return false;
+            }
+            i++;
+            j++;
+        }
+
+        i=rows;
+        j=cols;
+        while (i<n && j>=0){
+            if (arr[i][j]=='Q'){
+                return false;
+            }
+            i++;
+            j--;
+        }
+
+        i=rows;
+        j=cols;
+        while (i>=0 && j>=0){
+            if (arr[i][j]=='Q'){
+                return false;
+            }
+            i--;
+            j--;
+        }
+
+        return true;
+    }
+}
