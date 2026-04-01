65ae10ba1042e2658b84b913cf3a17c68e9ce224
Jasnoor Singh <jasnoortoor1984@gmail.com>
2026-04-01 13:45:47 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Parenthesis Checker/parenthesis-checker.java
+++ Difficulty: Easy/Parenthesis Checker/parenthesis-checker.java
@@ -1,24 +1,23 @@
 class Solution {
     public boolean isBalanced(String s) {
-        // code here
         Stack<Character> st=new Stack<>();
-        
         for (int i=0;i<s.length();i++){
             char ch=s.charAt(i);
             
             if (ch=='[' || ch=='{' || ch=='('){
                 st.push(ch);
-            }else{
+            }else {
                 if (st.isEmpty()){
                     return false;
                 }
                 
-                char top=st.pop();
-                if ((top=='[' && ch!=']') ||
-                    (top=='{' && ch!='}') ||
-                    (top=='(' && ch!=')') ){
+                int top=st.pop();
+                
+                if ((ch==']' && top!='[')||
+                    (ch=='}' && top!='{')||
+                    (ch==')' && top!='(')){
                         return false;
-                }
+                    }
             }
         }
         
