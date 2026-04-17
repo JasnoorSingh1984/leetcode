fb74e94ee2c8fcfc053648cdef59e0e97e94fcbd
Jasnoor Singh <jasnoortoor1984@gmail.com>
2026-04-18 00:27:53 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Permutations of a String/permutations-of-a-string.java
+++ Difficulty: Medium/Permutations of a String/permutations-of-a-string.java
@@ -1,29 +1,38 @@
 class Solution {
     public ArrayList<String> findPermutation(String s) {
-        // Code here
-        Set<String> set=new HashSet<>();
+        char[] nums=s.toCharArray();
         ArrayList<String> arr=new ArrayList<>();
-        
-        print(s,"",set);
-        
-        ArrayList<String> ans=new ArrayList<>(set);
-        Collections.sort(ans);
-        
-        return ans;
+        subs(nums,0,arr);
+        Collections.sort(arr);
+        return arr;
     }
     
-    public void print(String str,String s,Set<String> arr){
-        if (str.equals("")){
-            arr.add(s);
+    public void subs(char[] nums,int idx,ArrayList<String> arr){
+        if (idx==nums.length){
+            StringBuilder sb=new StringBuilder();
+            for (int i=0;i<nums.length;i++){
+                sb.append(nums[i]);
+            }
+            arr.add(sb.toString());
             return;
         }
         
-        for (int i=0;i<str.length();i++){
-            char ch=str.charAt(i);
-            String left=str.substring(0,i);
-            String right=str.substring(i+1);
-            String reminder=left+right;
-            print(reminder,s+ch,arr);
+        HashSet<Character> set=new HashSet<>();
+        for (int i=idx;i<nums.length;i++){
+            if (set.contains(nums[i])){
+                continue;
+            }
+            
+            set.add(nums[i]);
+            swap(nums,i,idx);
+            subs(nums,idx+1,arr);
+            swap(nums,i,idx);
         }
+    }
+    
+    public void swap(char[] nums,int i,int idx){
+        char temp=nums[i];
+        nums[i]=nums[idx];
+        nums[idx]=temp;
     }
 }
