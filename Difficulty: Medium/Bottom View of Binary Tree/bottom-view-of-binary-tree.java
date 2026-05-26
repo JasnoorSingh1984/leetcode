9d389a08e2b9557b30c8ce429ed5914bdb394ca5
Jasnoor Singh <jasnoortoor1984@gmail.com>
2026-05-26 11:32:28 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Bottom View of Binary Tree/bottom-view-of-binary-tree.java
+++ Difficulty: Medium/Bottom View of Binary Tree/bottom-view-of-binary-tree.java
@@ -6,7 +6,6 @@ class Solution {
         }
         
         TreeMap<Integer,Integer> map=new TreeMap<>();
-        
         Queue<Node> nodeq=new LinkedList<>();
         Queue<Integer> hdq=new LinkedList<>();
         nodeq.add(root);
@@ -16,16 +15,13 @@ class Solution {
             Node curr=nodeq.remove();
             int hd=hdq.remove();
             
-            if (!map.containsKey(hd)){
-                map.put(hd,curr.data);
-            }else{
-                map.put(hd,curr.data);
-            }
+            map.put(hd,curr.data);
             
             if (curr.left!=null){
                 nodeq.add(curr.left);
                 hdq.add(hd-1);
             }
+            
             if (curr.right!=null){
                 nodeq.add(curr.right);
                 hdq.add(hd+1);
