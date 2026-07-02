8f19d6db2c7cc27197710123371843efbb4a2ba6
Jasnoor Singh <jasnoortoor1984@gmail.com>
2026-07-02 16:33:36 +0530

    Time: 24 ms (5.91%), Space: 46.4 MB (33.58%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -4,18 +4,30 @@ class Solution {
             return false;
         }
 
-        char[] st=s.toCharArray();
-        char[] tt=t.toCharArray();
-
-        Arrays.sort(st);
-        Arrays.sort(tt);
-
-        for (int i=0;i<st.length;i++){
-            if (st[i]!=tt[i]){
-                return false;
+        HashMap<Character,Integer> map=new HashMap<>();
+        for (int i=0;i<s.length();i++){
+            if (map.containsKey(s.charAt(i))){
+                int freq=map.get(s.charAt(i));
+                map.put(s.charAt(i),freq+1);
+            }else{
+                map.put(s.charAt(i),1);
             }
         }
 
-        return true;        
+        for (int i=0;i<t.length();i++){
+            char ch=t.charAt(i);
+            if (!map.containsKey(ch)){
+                return false;
+            }else{
+                int freq=map.get(ch);
+                if (freq==1){
+                    map.remove(ch);
+                }else{
+                    map.put(ch,freq-1);
+                }
+            }
+        }
+
+        return map.isEmpty();
     }
 }
