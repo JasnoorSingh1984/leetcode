b34ce4116a22b40c75bfe95f15e7999b269bbaa1
Jasnoor Singh <jasnoortoor1984@gmail.com>
2026-07-02 16:50:41 +0530

    Time: 6 ms (43.99%), Space: 44.1 MB (89.54%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -5,15 +5,14 @@ class Solution {
         }
         
         int[] freq1=new int[26];
-        int[] freq2=new int[26];
 
         for (int i=0;i<s.length();i++){
             freq1[s.charAt(i)-'a']++;
-            freq2[t.charAt(i)-'a']++;
+            freq1[t.charAt(i)-'a']--;
         }
 
         for (int i=0;i<26;i++){
-            if (freq1[i]!=freq2[i]){
+            if (freq1[i]!=0){
                 return false;
             }
         }
