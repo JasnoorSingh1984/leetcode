fc4e2bd24b3300aeb02663a70b518d18aab91d2a
Jasnoor Singh <jasnoortoor1984@gmail.com>
2026-07-05 10:31:26 +0530

    Time: 13 ms (36.07%), Space: 46 MB (37.41%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -5,13 +5,14 @@ class Solution {
         int left=0;
         int right=str.length()-1;
 
-        while(left<right){
-            if(str.charAt(left)!=str.charAt(right)){
+        while (left<right){
+            if (str.charAt(left)!=str.charAt(right)){
                 return false;
             }
             left++;
             right--;
         }
+
         return true;
     }
 }
