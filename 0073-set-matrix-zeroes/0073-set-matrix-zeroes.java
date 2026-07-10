7e5e3b55e1c801b575d11c6f57fcc33d29c10ec6
Jasnoor Singh <jasnoortoor1984@gmail.com>
2026-07-10 12:02:35 +0530

    Time: 1 ms (98.57%), Space: 47.3 MB (95.73%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,35 +1,23 @@
 class Solution {
-    public void setrow(int[][] arr,int i){
-        for (int j=0;j<arr[0].length;j++){
-            if (arr[i][j]!=0){
-                arr[i][j]=-10;
-            }
-        }
-    }
-
-    public void setcol(int[][] arr,int j){
-        for (int i=0;i<arr.length;i++){
-            if (arr[i][j]!=0){
-                arr[i][j]=-10;
-            }
-        }
-    }
     public void setZeroes(int[][] matrix) {
         int n=matrix.length;
         int m=matrix[0].length;
 
+        int[] row=new int[n];
+        int[] cols=new int[m];
+
         for (int i=0;i<n;i++){
             for (int j=0;j<m;j++){
                 if (matrix[i][j]==0){
-                    setrow(matrix,i);
-                    setcol(matrix,j);
+                    row[i]=1;
+                    cols[j]=1;
                 }
             }
         }
 
         for (int i=0;i<n;i++){
             for (int j=0;j<m;j++){
-                if (matrix[i][j]==-10){
+                if (row[i]==1 || cols[j]==1){
                     matrix[i][j]=0;
                 }
             }
