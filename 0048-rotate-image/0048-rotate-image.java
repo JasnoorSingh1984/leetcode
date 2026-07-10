8b2e78e652a46d7528706553be8787c64c7fdab7
Jasnoor Singh <jasnoortoor1984@gmail.com>
2026-07-10 13:30:34 +0530

    Time: 0 ms (100%), Space: 43.8 MB (34.43%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -1,17 +1,23 @@
 class Solution {
-    public void rotate(int[][] nums) {
-        int n=nums.length;
+    public void rotate(int[][] arr) {
+        int n=arr.length;
 
-        int[][] arr=new int[n][n];
         for (int i=0;i<n;i++){
-            for (int j=0;j<n;j++){
-                arr[j][n-i-1]=nums[i][j];
+            for (int j=i+1;j<n;j++){
+                int temp=arr[i][j];
+                arr[i][j]=arr[j][i];
+                arr[j][i]=temp;
             }
         }
 
         for (int i=0;i<n;i++){
-            for (int j=0;j<n;j++){
-                nums[i][j]=arr[i][j];
+            int left=0,right=n-1;
+            while (left<right){
+                int temp=arr[i][left];
+                arr[i][left]=arr[i][right];
+                arr[i][right]=temp;
+                left++;
+                right--;
             }
         }
     }
