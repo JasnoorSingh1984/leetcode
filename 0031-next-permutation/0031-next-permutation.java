91164fd1dd2e00293f364f8373e4c2112df75741
Jasnoor Singh <jasnoortoor1984@gmail.com>
2026-07-13 23:18:50 +0530

    Time: 0 ms (100%), Space: 44.7 MB (51.59%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -1,6 +1,6 @@
 class Solution {
     public void reverse(int[] nums,int left,int right){
-        while(left<right){
+        while (left<right){
             int temp=nums[left];
             nums[left]=nums[right];
             nums[right]=temp;
@@ -10,6 +10,7 @@ class Solution {
     }
     public void nextPermutation(int[] nums) {
         int idx=-1;
+
         for (int i=nums.length-2;i>=0;i--){
             if (nums[i]<nums[i+1]){
                 idx=i;
@@ -24,15 +25,13 @@ class Solution {
 
         for (int i=nums.length-1;i>idx;i--){
             if (nums[i]>nums[idx]){
-                int temp=nums[i];
-                nums[i]=nums[idx];
-                nums[idx]=temp;
+                int temp=nums[idx];
+                nums[idx]=nums[i];
+                nums[i]=temp;
                 break;
             }
         }
 
-
         reverse(nums,idx+1,nums.length-1);
     }
-
 }
