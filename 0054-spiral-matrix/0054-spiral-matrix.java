d2e454a20cc6180eb15c9214ac8fd09182c0517d
Jasnoor Singh <jasnoortoor1984@gmail.com>
2026-07-13 23:10:25 +0530

    Time: 0 ms (100%), Space: 42.8 MB (90.99%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.java
+++ 0054-spiral-matrix/0054-spiral-matrix.java
@@ -3,35 +3,33 @@ class Solution {
         int left=0,right=arr[0].length-1;
         int top=0,bottom=arr.length-1;
 
-        List<Integer> list=new ArrayList<>();
-
-
-        while (left<=right && top<=bottom){
+        ArrayList<Integer> nums=new ArrayList<>();
+        while (top<=bottom && left<=right){
             for (int i=left;i<=right;i++){
-                list.add(arr[top][i]);
+                nums.add(arr[top][i]);
             }
             top++;
 
             for (int i=top;i<=bottom;i++){
-                list.add(arr[i][right]);
+                nums.add(arr[i][right]);
             }
             right--;
 
             if (top<=bottom){
                 for (int i=right;i>=left;i--){
-                    list.add(arr[bottom][i]);
+                    nums.add(arr[bottom][i]);
                 }
                 bottom--;
             }
 
             if (left<=right){
                 for (int i=bottom;i>=top;i--){
-                    list.add(arr[i][left]);
+                    nums.add(arr[i][left]);
                 }
                 left++;
             }
         }
 
-        return list;
+        return nums;
     }
 }
