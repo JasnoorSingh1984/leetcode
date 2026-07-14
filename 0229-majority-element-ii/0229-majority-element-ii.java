fb7288c38788b383293cff2c6f1f393ca25f23c0
Jasnoor Singh <jasnoortoor1984@gmail.com>
2026-07-14 10:20:17 +0530

    Time: 2 ms (99.95%), Space: 52.4 MB (75.81%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -12,11 +12,11 @@ class Solution {
             }else if (nums[i]==ele2){
                 count2++;
             }else if (count1==0){
-                count1=1;
                 ele1=nums[i];
+                count1=1;
             }else if (count2==0){
-                count2=1;
                 ele2=nums[i];
+                count2=1;
             }else{
                 count1--;
                 count2--;
@@ -34,15 +34,16 @@ class Solution {
             }
         }
 
-        List<Integer> list=new ArrayList<>();
+        List<Integer> l=new ArrayList<>();
 
         if (count1>nums.length/3){
-            list.add(ele1);
-        }
-        if (count2>nums.length/3){
-            list.add(ele2);
+            l.add(ele1);
         }
 
-        return list;
+        if (count2>nums.length/3){
+            l.add(ele2);
+        }
+
+        return l;
     }
 }
