75bae57acedd459d83f8524c7b0c1bcc68ae5d0d
Jasnoor Singh <jasnoortoor1984@gmail.com>
2026-07-20 12:41:13 +0530

    Time: 32 ms (78.76%), Space: 60.4 MB (7.94%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,40 +1,44 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         Arrays.sort(nums);
+
         List<List<Integer>> arr=new ArrayList<>();
 
         for (int i=0;i<nums.length;i++){
             if (i>0 && nums[i]==nums[i-1]){
                 continue;
             }
-            int j=i+1;
-            int k=nums.length-1;
 
-            while (j<k){
-                int sum=nums[i]+nums[j]+nums[k];
+                int j=i+1;
+                int k=nums.length-1;
 
-                if (sum==0){
-                    List<Integer> l=new ArrayList<>();
-                    l.add(nums[i]);
-                    l.add(nums[j]);
-                    l.add(nums[k]);
-                    arr.add(l);
-                    j++;
-                    k--;
+                while (j<k){
+                    int sum=nums[i]+nums[j]+nums[k];
 
-                    while (j<k && nums[j]==nums[j-1]){
+                    if (sum==0){
+                        List<Integer> list=new ArrayList<>();
+                        list.add(nums[i]);
+                        list.add(nums[j]);
+                        list.add(nums[k]);
+
+                        arr.add(list);
                         j++;
-                    }
-                    while (j<k && nums[k]==nums[k+1]){
+                        k--;
+
+                        while (j<k && nums[j]==nums[j-1]){
+                            j++;
+                        }
+                        while (j<k && nums[k]==nums[k+1]){
+                            k--;
+                        }
+                    }else if(sum<0){
+                        j++;
+                    }else{
                         k--;
                     }
-                }else if (sum<0){
-                    j++;
-                }else{
-                    k--;
                 }
             }
-        }
-        return arr;
+
+        return arr;        
     }
 }
