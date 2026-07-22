29576d268aec5210debe5b8c90c10839ec2ee01e
Jasnoor Singh <jasnoortoor1984@gmail.com>
2026-07-22 12:02:37 +0530

    Time: 22 ms (6.46%), Space: 43 MB (8.34%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,5 +1,14 @@
 class Solution {
     public int mySqrt(int x) {
-        return (int) Math.sqrt(x);
+        int ans=0;
+        for (int i=1;i<=x;i++){
+            if ((long) i*i <= x){
+                ans=i;
+            }else{
+                break;
+            }
+        }
+
+        return ans;
     }
 }
