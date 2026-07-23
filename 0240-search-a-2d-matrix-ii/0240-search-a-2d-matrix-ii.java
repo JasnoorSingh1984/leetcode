7e0320392bce9345045ef6c14769feade71b6560
Jasnoor Singh <jasnoortoor1984@gmail.com>
2026-07-23 17:31:20 +0530

    Time: 4 ms (23.41%), Space: 48.2 MB (45.1%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
@@ -1,9 +1,18 @@
 class Solution {
     public boolean searchMatrix(int[][] nums, int target) {
         for (int i=0;i<nums.length;i++){
-            for (int j=0;j<nums[0].length;j++){
-                if (nums[i][j]==target){
+            int low=0;
+            int high=nums[i].length-1;
+
+            while (low<=high){
+                int mid=low+(high-low)/2;
+
+                if (nums[i][mid]==target){
                     return true;
+                }else if (nums[i][mid]<target){
+                    low=mid+1;
+                }else{
+                    high=mid-1;
                 }
             }
         }
