09d66e58a8271ed122592dfac8593c24d6022768
Jasnoor Singh <jasnoortoor1984@gmail.com>
2026-07-24 11:52:44 +0530

    Time: 5 ms (82.81%), Space: 77.3 MB (49.18%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -6,8 +6,8 @@ class Solution {
         int right=nums.length-1;
         while (left<right){
             int min=Math.min(nums[left],nums[right]);
-            max=Math.max(max,min*(right-left));
 
+            max=Math.max(max,min*(right-left));
             if (nums[left]<nums[right]){
                 left++;
             }else{
