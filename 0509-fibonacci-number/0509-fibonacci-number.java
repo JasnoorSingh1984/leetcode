8f1f78adb528a78e3ce6bf10bb4b51b7a295e741
Jasnoor Singh <jasnoortoor1984@gmail.com>
2026-09-09 09:45:24 +0530

    Time: 0 ms (100%), Space: 41.7 MB (96.73%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -0,0 +1,22 @@
+class Solution {
+    static int[] dp;
+    public int helper(int n){
+        if (n<=1){
+            return n;
+        }
+
+        if (dp[n]!=0){
+            return dp[n];
+        }
+
+        int ans=helper(n-1)+helper(n-2);
+
+        dp[n]=ans;
+        return ans;
+    }
+
+    public int fib(int n) {
+        dp=new int[n+1];
+        return helper(n);
+    }
+}
