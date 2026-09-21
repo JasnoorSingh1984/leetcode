abcbd2c984d9975bef9b708ad6c2450e2f69c3bc
Jasnoor Singh <jasnoortoor1984@gmail.com>
2026-09-21 11:37:20 +0530

    Time: 1 ms (99.98%), Space: 45.5 MB (38.86%) - LeetHub

--- 0113-path-sum-ii/0113-path-sum-ii.java
+++ 0113-path-sum-ii/0113-path-sum-ii.java
@@ -2,28 +2,27 @@ class Solution {
     public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
         List<List<Integer>> arr=new ArrayList<>();
         List<Integer> l=new ArrayList<>();
-        check(root,targetSum,arr,l);
+        path(root,targetSum,arr,l);
         return arr;
     }
 
-    public void check(TreeNode root,int tar,List<List<Integer>> arr,List<Integer> l){
+    public void path(TreeNode root,int sum,List<List<Integer>> arr,List<Integer> l){
         if (root==null){
+            return ;
+        }
+
+        if (root.left==null && root.right==null){
+            if (sum==root.val){
+                l.add(root.val);
+                arr.add(new ArrayList<>(l));
+                l.remove(l.size()-1);
+            }
             return;
         }
 
         l.add(root.val);
-
-        if (root.left==null && root.right==null){
-            if (tar==root.val){
-                arr.add(new ArrayList<>(l));
-            }
-            l.remove(l.size()-1);
-            return;
-        }
-
-        check(root.left,tar-root.val,arr,l);
-        check(root.right,tar-root.val,arr,l);
-
+        path(root.left,sum-root.val,arr,l);
+        path(root.right,sum-root.val,arr,l);
         l.remove(l.size()-1);
     }
 }
