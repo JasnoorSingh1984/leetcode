7eb873d5d00dddfa4743171c9d86c96bae468a94
Jasnoor Singh <jasnoortoor1984@gmail.com>
2026-09-22 00:02:32 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Top View of Binary Tree/top-view-of-binary-tree.java
+++ Difficulty: Medium/Top View of Binary Tree/top-view-of-binary-tree.java
@@ -2,17 +2,12 @@ class Solution {
     public ArrayList<Integer> topView(Node root) {
         ArrayList<Integer> arr=new ArrayList<>();
         
-        if (root==null){
-            return arr;
-        }
-        
         TreeMap<Integer,Integer> map=new TreeMap<>();
         Queue<Node> nodeq=new LinkedList<>();
         Queue<Integer> posq=new LinkedList<>();
         
         nodeq.add(root);
         posq.add(0);
-        
         while (!nodeq.isEmpty()){
             Node curr=nodeq.remove();
             int pos=posq.remove();
