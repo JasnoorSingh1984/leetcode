a0e5f5c5076e34a67200fe80699bd783ac7b4b74
Jasnoor Singh <jasnoortoor1984@gmail.com>
2026-09-21 23:52:28 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Left View of Binary Tree/left-view-of-binary-tree.java
+++ Difficulty: Easy/Left View of Binary Tree/left-view-of-binary-tree.java
@@ -1,21 +1,25 @@
 class Solution {
     public ArrayList<Integer> leftView(Node root) {
-        ArrayList<Integer> arr=new ArrayList<>();
+        TreeMap<Integer,Integer> map=new TreeMap<>();
+        view(root,0,map);
         
-        view(root,0,arr);
+        ArrayList<Integer> arr=new ArrayList<>();
+        for (int val:map.values()){
+            arr.add(val);
+        }
         return arr;
     }
     
-    public void view(Node root,int level,ArrayList<Integer> arr){
+    public void view(Node root,int level,TreeMap<Integer,Integer> map){
         if (root==null){
             return;
         }
         
-        if (level>=arr.size()){
-            arr.add(root.data);
+        if (!map.containsKey(level)){
+            map.put(level,root.data);
         }
         
-        view(root.left,level+1,arr);
-        view(root.right,level+1,arr);
+        view(root.left,level+1,map);
+        view(root.right,level+1,map);
     }
 }
