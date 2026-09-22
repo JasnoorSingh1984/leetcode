76bd929110eb202af56e92f75265dad52516d151
Jasnoor Singh <jasnoortoor1984@gmail.com>
2026-09-22 09:56:24 +0530

    Time: 15 ms (89.34%), Space: 70 MB (17.27%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
@@ -9,12 +9,10 @@ class Solution {
 
         if (left!=null && right!=null){
             return root;
-        }
-
-        if (left!=null){
-            return left;
-        }else{
+        }else if (left==null){
             return right;
+        }else{
+            return left;
         }
     }
 }
