2f15f4616c75198b2550c11f656a7407a4a30f03
Jasnoor Singh <jasnoortoor1984@gmail.com>
2026-09-23 12:44:40 +0530

    Time: 4 ms (5.23%), Space: 43.2 MB (14.48%) - LeetHub

--- 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.java
+++ 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.java
@@ -1,26 +1,23 @@
 class Solution {
     public int sumNumbers(TreeNode root) {
         List<Integer> arr=new ArrayList<>();
-        paths(root,arr,"");
+        
+        int[] ans=new int[1];
+        paths(root,arr,"",ans);
 
-        int sum=0;
-        for (int i=0;i<arr.size();i++){
-            sum+=arr.get(i);
-        }
-
-        return sum;
+        return ans[0];
     }
 
-    public void paths(TreeNode root,List<Integer> arr,String str){
+    public void paths(TreeNode root,List<Integer> arr,String str,int[] ans){
         if (root==null) return;
 
         if (root.left==null && root.right==null){
             str=str+root.val;
-            arr.add(Integer.parseInt(str));
+            ans[0]+=Integer.parseInt(str);
             return;
         }
 
-        paths(root.left,arr,str+root.val);
-        paths(root.right,arr,str+root.val);
+        paths(root.left,arr,str+root.val,ans);
+        paths(root.right,arr,str+root.val,ans);
     }
 }
