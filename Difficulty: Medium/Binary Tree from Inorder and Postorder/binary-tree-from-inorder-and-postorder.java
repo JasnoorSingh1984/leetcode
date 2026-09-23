27579de199f9f86c091adabffeb9c5868e8c94f1
Jasnoor Singh <jasnoortoor1984@gmail.com>
2026-09-23 10:49:05 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Binary Tree from Inorder and Postorder/binary-tree-from-inorder-and-postorder.java
@@ -0,0 +1,27 @@
+class Solution {
+    public Node buildTree(int[] inorder, int[] postorder) {
+        int n=inorder.length;
+        return build(postorder,inorder,0,n-1,0,n-1);
+    }
+    
+    public Node build(int[] postorder,int[] inorder,int plow,int phigh,int ilow,int ihigh){
+        if (plow>phigh) return null;
+        
+        int val=postorder[phigh];
+        Node root=new Node(val);
+        
+        int r=-1;
+        for (int i=ilow;i<=ihigh;i++){
+            if (inorder[i]==val){
+                r=i;
+                break;
+            }
+        }
+        int cnt=r-ilow;
+        
+        root.left=build(postorder,inorder,plow,plow+cnt-1,ilow,r-1);
+        root.right=build(postorder,inorder,plow+cnt,phigh-1,r+1,ihigh);
+        
+        return root;
+    }
+}
