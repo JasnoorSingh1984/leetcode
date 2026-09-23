c16ae660062aa274c9879c6a271291998be4bd0c
Jasnoor Singh <jasnoortoor1984@gmail.com>
2026-09-23 11:53:50 +0530

    Time: 2 ms (97.76%), Space: 48.3 MB (88.92%) - LeetHub

--- 0637-average-of-levels-in-binary-tree/0637-average-of-levels-in-binary-tree.java
+++ 0637-average-of-levels-in-binary-tree/0637-average-of-levels-in-binary-tree.java
@@ -1,29 +1,20 @@
 class Solution {
     public List<Double> averageOfLevels(TreeNode root) {
-        List<List<Integer>> arr=new ArrayList<>();
+        List<Double> ans=new ArrayList<>();
+
         Queue<TreeNode> q=new LinkedList<>();
         q.add(root);
         while (!q.isEmpty()){
             int size=q.size();
-            List<Integer> l=new ArrayList<>();
+            double sum=0;
             for (int i=0;i<size;i++){
                 TreeNode curr=q.remove();
-                l.add(curr.val);
+                sum+=curr.val;
 
                 if (curr.left!=null) q.add(curr.left);
                 if (curr.right!=null) q.add(curr.right);
             }
-
-            arr.add(l);
-        }
-
-        List<Double> ans=new ArrayList<>();
-        for (List<Integer> l:arr){
-            double sum=0;
-            for (int i=0;i<l.size();i++){
-                sum+=l.get(i);
-            }
-            double avg=sum/l.size();
+            double avg=sum/size;
             ans.add(avg);
         }
 
